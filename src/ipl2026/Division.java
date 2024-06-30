726c9f3d7a5a0e2ab2301ea40ee2d8e7e065f9e3
neoteric <asampathkumar545@gmail.com>
2024-06-30 11:55:22 +0530

    division two numbers

---
+++ src/ipl2026/Division.java
@@ -0,0 +1,11 @@
+package ipl2026;
+
+public class Division {
+
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 20;
+        int c = a * b;
+        System.out.println("adding two numbers " + c);
+    }
+}
