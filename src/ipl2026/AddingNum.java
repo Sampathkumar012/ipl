2dc8d458e201648ae99086923fc9b8250ecfb10f
neoteric <asampathkumar545@gmail.com>
2024-06-30 10:28:59 +0530

    adding two numbers

---
+++ src/ipl2026/AddingNum.java
@@ -0,0 +1,10 @@
+package ipl2026;
+
+public class AddingNum {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        int c=a+b;
+        System.out.println("adding two numbers "+c);
+    }
+}
