85f774822a6a42c47cb3a294ade2d2e1c35b816f
neoteric <asampathkumar545@gmail.com>
2024-06-30 12:13:07 +0530

    division- siri

---
+++ src/ipl2026/Division.java
@@ -0,0 +1,12 @@
+
+public class Division {
+
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 20;
+        int c = a * b;
+        System.out.println("adding two numbers " + c);
+        System.out.println("a and b are integers");
+       System.out.println("merge conflits ");
+    }
+}
