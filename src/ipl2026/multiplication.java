cdae11849f001ca29c533f9dd85a47e05ca0701e
Sampathkumar012 <asampathkumar545@gmail.com>
2024-06-30 12:01:05 +0530

    Feature2 (#2)
    * multi two numbers
    
    * division two numbers
    
    * division- siri

---
+++ src/ipl2026/multiplication.java
@@ -0,0 +1,11 @@
+package ipl2026;
+
+public class multiplication {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = a * b;
+        System.out.println("muitiplication two numbers "+  c);
+    }
+}
