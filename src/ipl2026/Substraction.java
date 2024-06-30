a8316e38551073108074bfa4eee3e26817f8bc8d
neoteric <asampathkumar545@gmail.com>
2024-06-30 10:28:59 +0530

    sub two numbers

---
+++ src/ipl2026/Substraction.java
@@ -0,0 +1,13 @@
+package ipl2026;
+
+public class Substraction {
+
+    public static void main(String[] args) {
+
+
+        int a = 10;
+        int b = 20;
+        int c = b - a;
+        System.out.println("substraction two numbers ");
+    }
+}
